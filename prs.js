87c17f8980a456306c655904bab19ef82312d00a
Sebastian Vidal Aedo <sebastianvidalaedo@gmail.com>
2024-12-11 01:39:51 -0300

    add auto prs

--- prs.js
+++ prs.js
@@ -14,7 +14,8 @@ function generateBranchName() {
   const day = String(now.getDate()).padStart(2, '0');
   const hours = String(now.getHours()).padStart(2, '0');
   const minutes = String(now.getMinutes()).padStart(2, '0');
-  return `feat/branch-${year}${month}${day}-${hours}${minutes}`;
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  return `feat/branch-${year}${month}${day}-${hours}${minutes}${seconds}`;
 }
 
 // Función para ejecutar comandos de Git
